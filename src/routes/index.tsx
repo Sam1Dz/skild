03d121b0cb27f713d62370d65226df55050a32d4
Pratama Dimas <pratamadimas.pw@gmail.com>
2026-09-04 14:16:53 +0700

    ✨ feat: add featured skills scaffolding to home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router';
+
+import FeaturedSkills from '@/components/pages/home/featured';
 import Hero from '@/components/pages/home/hero';
 
 export const Route = createFileRoute('/')({ component: Home });
 
 function Home() {
-	return <Hero />;
+	return (
+		<>
+			<Hero />
+			<FeaturedSkills />
+		</>
+	);
 }
